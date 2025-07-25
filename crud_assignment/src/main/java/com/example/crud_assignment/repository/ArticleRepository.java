d9e4fe6b9b305d932a0196c02c3d09a7414d2905
Sajaltripathi07 <136965660+Sajaltripathi07@users.noreply.github.com>
2025-07-25 18:07:17 +0530

    Update ArticleRepository.java
    Signed-off-by: Sajaltripathi07 <136965660+Sajaltripathi07@users.noreply.github.com>

--- crud_assignment/src/main/java/com/example/crud_assignment/repository/ArticleRepository.java
+++ crud_assignment/src/main/java/com/example/crud_assignment/repository/ArticleRepository.java
@@ -1,9 +1,12 @@
 package com.example.crud_assignment.repository;
 
 import com.example.crud_assignment.model.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
+    Page<Article> findByUserId(Long userId, Pageable pageable);
 }
